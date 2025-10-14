b870745265ba9b301e5ed2d73f45defa433135b7
saalim-km <saalimkm@gmail.com>
2025-10-14 10:29:24 +0530

    Remove multiple unused image files and update package configurations
    - Deleted numerous unused image files from the uploads directory.
    - Updated tsconfig.json to remove the "moduleResolution" property.
    - Modified client package-lock.json to mark several dependencies as development dependencies.
    - Added a new package-lock.json file for the root project.

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -1,18 +1,19 @@
-import express, { type Application } from "express"
+import express, { type Application } from "express";
 import routes from "./routes/index";
 import cors from "cors";
 import morgan from "morgan";
 
-
 const app: Application = express();
 
 //middlewares
-app.use(cors({
+app.use(
+  cors({
     origin: process.env.ALLOWED_ORIGIN,
     methods: ["GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS"],
     allowedHeaders: ["Authorization", "Content-Type"],
     credentials: true,
-}));
+  })
+);
 app.use(morgan("dev"));
 app.use(express.json());
 
